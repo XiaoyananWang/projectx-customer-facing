8551bbb5f62b2db0c8e25b4d84051de71f7e2373
wxyn <100791680+XiaoyananWang@users.noreply.github.com>
2023-07-28 13:41:35 -0400

    adding styles using MUI and account page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,27 +1,42 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link as RouterLink } from 'react-router-dom';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 
 const NavBar = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/account">Account</Link>
-        </li>
-        <li>
-          <Link to="/threads">Threads</Link>
-        </li>
-        <li>
-          <Link to="/signup">Sign Up</Link>
-        </li>
-        <li>
-          <Link to="/signin">Log In</Link>
-        </li>
-      </ul>
-    </nav>
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton edge="start" color="inherit" aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          MyApp
+        </Typography>
+        <Box sx={{ display: 'flex', gap: 2 }}>
+          <Button color="inherit" component={RouterLink} to="/">
+            Home
+          </Button>
+          <Button color="inherit" component={RouterLink} to="/account">
+            Account
+          </Button>
+          <Button color="inherit" component={RouterLink} to="/threads">
+            Threads
+          </Button>
+          <Button color="inherit" component={RouterLink} to="/signup">
+            Sign Up
+          </Button>
+          <Button color="inherit" component={RouterLink} to="/signin">
+            Log In
+          </Button>
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 };
 
--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,11 +1,104 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import IconButton from '@mui/material/IconButton';
+import EditIcon from '@mui/icons-material/Edit';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-const Account = () => {
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1976d2',
+    },
+    secondary: {
+      main: '#dc004e',
+    },
+  },
+});
+
+const AccountPage = () => {
+  const [avatar, setAvatar] = useState(null);
+  const [username, setUsername] = useState('Elva');
+  const [editingUsername, setEditingUsername] = useState(false);
+
+  const handleSignOut = () => {
+    console.log('Signing out...');
+  };
+
+  const handleDeleteAccount = () => {
+    console.log('Deleting account...');
+  };
+
+  const handleAvatarChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setAvatar(URL.createObjectURL(file));
+    }
+  };
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const toggleEditingUsername = () => {
+    setEditingUsername(!editingUsername);
+  };
+
   return (
-    <div>
-        <h1>This is the Account Page</h1>
-    </div>
-  )
-}
+    <ThemeProvider theme={theme}>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          gap: 2,
+          bgcolor: 'background.default',
+          minHeight: '100vh',
+          padding: 4,
+        }}
+      >
+        <Avatar alt={username} src={avatar} />
+        {editingUsername ? (
+          <TextField
+            label="Username"
+            value={username}
+            onChange={handleUsernameChange}
+            variant="outlined"
+            onBlur={toggleEditingUsername}
+          />
+        ) : (
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <Typography variant="h6">{username}</Typography>
+            <IconButton onClick={toggleEditingUsername} size="small">
+              <EditIcon />
+            </IconButton>
+          </Box>
+        )}
+        <Typography variant="body1">elva@example.com</Typography>
+        <input
+          accept="image/*"
+          style={{ display: 'none' }}
+          id="avatar-upload"
+          type="file"
+          onChange={handleAvatarChange}
+        />
+        <label htmlFor="avatar-upload">
+          <Button variant="contained" color="primary" component="span">
+            Change Avatar
+          </Button>
+        </label>
+        <Button variant="contained" color="primary" onClick={handleSignOut}>
+          Sign Out
+        </Button>
+        <Button variant="contained" color="secondary" onClick={handleDeleteAccount}>
+          Delete Account
+        </Button>
+      </Box>
+    </ThemeProvider>
+  );
+};
 
-export default Account
+export default AccountPage;
